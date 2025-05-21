a809cce73124f7041bb87a6d76fa4cf3a7db9f76
itsynkjr <kingsergio1738@gmail.com>
2025-05-20 21:16:15 -0700

    Update index.py

--- api /index.py
+++ api /index.py
@@ -1,11 +1,6 @@
-from http.server import BaseHTTPRequestHandler
-import json
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','application/json')
-        self.end_headers()
-        message = {'message': 'Backend is working!'}
-        self.wfile.write(bytes(json.dumps(message), "utf8"))
-        return
+def handler(request):
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": '{"message": "Music WRLD backend is working!"}'
+    }
